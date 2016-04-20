638ea39f22170320597e83eed8aacfa70191b615
freepina <freepina@gmail.com>
2016-04-20 01:39:44 -0400

    sdfd

--- picmake.c
+++ picmake.c
@@ -1,24 +1,41 @@
 #include <fcntl.h>
-#include <stdin.h>
-#include <stdout.h>
-#include <strlib.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <errno.h>
+#include <math.h>
 
 int main(){
   int filey;
   int r, g, b;
-  char* header = "P3 100 100 255\n";
+  char *header = "P3 401 401 255\n";
+  char buffer[100];
 
-  filey = open( "newpix.ppm", O_CREAT | O_TRUNC | O_RDWR );
-
+  printf("%s",header);
+  filey = open( "newpix.ppm", O_CREAT | O_TRUNC | O_RDWR, 0666 );
+  if (filey == -1){
+    printf("error num %d: %s",errno, strerror(errno));
+    return errno;
+  }
   write (filey, header, strlen(header) );
-  
-  for (int i=0;i<100;i++){
-    for (int j=0;j<100;j++){
-      r += i;
-      r = r % 255;
-      g = (int)(r *
-      sprintf (filey, "%d %d %d", 
+  if (errno){
+    printf("error num %d: %s",errno, strerror(errno));
+    return errno;
+  }
+  for (int i=0;i<401;i++){
+    for (int j=0;j<401;j++){
+      r = sin((j)/5.)*cos((i)/5.)*128+127;
+      b = sin((j)/5.-15.7)*cos((i)/5.-15.7/2)*128+127;
+      g = sin((pow(j,2)+pow(i,2))/9)*128+127;
+      int si = sprintf(buffer, "%d %d %d ", (int)r, (int)g, (int)b);
+      write (filey, buffer, si);
+      if (errno){
+	printf("error num %d: %s",errno, strerror(errno));
+	return errno;
+      }
     }
   }
-
+  printf("successsssss :)))))))))");
+  close(filey);
 }
